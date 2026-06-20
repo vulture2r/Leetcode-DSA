4888fbad7c5486f04820019f6cd0846b6c7e2419
Sourov Mondal <souravmondal.byte@gmail.com>
2026-06-20 20:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (18.53%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,16 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-       seen = {} # empty dictionary
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
 
-       for i, num in enumerate(nums):
-        diff = target - num
-        
-        if diff in seen:
-            return [seen[diff], i]
-        seen[num] = i
-                
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return []
